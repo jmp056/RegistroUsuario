5f340ddf1f23402cdae45813707c8cd3bc039845
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-16 23:06:21 -0400

    Creando la entidad Usuario

---
+++ ProyectoFinal/Entidades/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int NivelDeUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public DateTime FechaDeIngreso { get; set; }
+
+        public Usuarios()
+        {
+            IdUsuario = 0;
+            Nombre = string.Empty;
+            Apellido = string.Empty;
+            NivelDeUsuario = 0;
+            Usuario = string.Empty;
+            Clave = string.Empty;
+            FechaDeIngreso = DateTime.Now;
+        }
+    }
+}
