f0bc89924e96278d45727638dc0dfc0d9411f0f7
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-17 02:31:51 -0400

    Terminando ventana de registro

---
+++ ProyectoFinal/UI/Registros/RegistroUsuarios.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace ProyectoFinal.UI.Registros
+{
+    partial class RegistroUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.IdUsuarioNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.ApellidoTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.UsuarioTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ClaveTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.FechaDeIngresoDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.MyErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.AdministradorCheckBox = new System.Windows.Forms.CheckBox();
+            this.SupervisorCheckBox = new System.Windows.Forms.CheckBox();
+            this.SoporteCheckBox = new System.Windows.Forms.CheckBox();
+            this.UsuarioCheckBox = new System.Windows.Forms.CheckBox();
+            this.NivelDeUsuarioGroupBox = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.IdUsuarioNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).BeginInit();
+            this.NivelDeUsuarioGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Location = new System.Drawing.Point(512, 64);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(75, 23);
+            this.BuscarButton.TabIndex = 0;
+            this.BuscarButton.Text = "Buscar";
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            this.BuscarButton.Click += new System.EventHandler(this.BuscarButton_Click);
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Location = new System.Drawing.Point(315, 377);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(75, 23);
+            this.NuevoButton.TabIndex = 1;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            this.NuevoButton.Click += new System.EventHandler(this.NuevoButton_Click);
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Location = new System.Drawing.Point(424, 377);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(75, 23);
+            this.GuardarButton.TabIndex = 2;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            this.GuardarButton.Click += new System.EventHandler(this.GuardarButton_Click);
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Location = new System.Drawing.Point(538, 376);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(75, 23);
+            this.EliminarButton.TabIndex = 3;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            this.EliminarButton.Click += new System.EventHandler(this.EliminarButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // IdUsuarioNumericUpDown
+            // 
+            this.IdUsuarioNumericUpDown.Location = new System.Drawing.Point(183, 31);
+            this.IdUsuarioNumericUpDown.Name = "IdUsuarioNumericUpDown";
+            this.IdUsuarioNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.IdUsuarioNumericUpDown.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Location = new System.Drawing.Point(183, 73);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NombreTextBox.TabIndex = 9;
+            // 
+            // ApellidoTextBox
+            // 
+            this.ApellidoTextBox.Location = new System.Drawing.Point(183, 99);
+            this.ApellidoTextBox.Name = "ApellidoTextBox";
+            this.ApellidoTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ApellidoTextBox.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(91, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "label4";
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(183, 125);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(100, 20);
+            this.EmailTextBox.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(91, 125);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            // 
+            // UsuarioTextBox
+            // 
+            this.UsuarioTextBox.Location = new System.Drawing.Point(209, 251);
+            this.UsuarioTextBox.Name = "UsuarioTextBox";
+            this.UsuarioTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UsuarioTextBox.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(117, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // ClaveTextBox
+            // 
+            this.ClaveTextBox.Location = new System.Drawing.Point(209, 277);
+            this.ClaveTextBox.Name = "ClaveTextBox";
+            this.ClaveTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ClaveTextBox.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(117, 277);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "label7";
+            // 
+            // FechaDeIngresoDateTimePicker
+            // 
+            this.FechaDeIngresoDateTimePicker.Location = new System.Drawing.Point(209, 324);
+            this.FechaDeIngresoDateTimePicker.Name = "FechaDeIngresoDateTimePicker";
+            this.FechaDeIngresoDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.FechaDeIngresoDateTimePicker.TabIndex = 18;
+            // 
+            // MyErrorProvider
+            // 
+            this.MyErrorProvider.ContainerControl = this;
+            // 
+            // AdministradorCheckBox
+            // 
+            this.AdministradorCheckBox.AutoSize = true;
+            this.AdministradorCheckBox.Location = new System.Drawing.Point(121, 53);
+            this.AdministradorCheckBox.Name = "AdministradorCheckBox";
+            this.AdministradorCheckBox.Size = new System.Drawing.Size(89, 17);
+            this.AdministradorCheckBox.TabIndex = 20;
+            this.AdministradorCheckBox.Text = "Administrador";
+            this.AdministradorCheckBox.UseVisualStyleBackColor = true;
+            this.AdministradorCheckBox.CheckedChanged += new System.EventHandler(this.AdministradorCheckBox_CheckedChanged);
+            // 
+            // SupervisorCheckBox
+            // 
+            this.SupervisorCheckBox.AutoSize = true;
+            this.SupervisorCheckBox.Location = new System.Drawing.Point(145, 30);
+            this.SupervisorCheckBox.Name = "SupervisorCheckBox";
+            this.SupervisorCheckBox.Size = new System.Drawing.Size(76, 17);
+            this.SupervisorCheckBox.TabIndex = 21;
+            this.SupervisorCheckBox.Text = "Supervisor";
+            this.SupervisorCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SoporteCheckBox
+            // 
+            this.SoporteCheckBox.AutoSize = true;
+            this.SoporteCheckBox.Location = new System.Drawing.Point(52, 57);
+            this.SoporteCheckBox.Name = "SoporteCheckBox";
+            this.SoporteCheckBox.Size = new System.Drawing.Size(63, 17);
+            this.SoporteCheckBox.TabIndex = 22;
+            this.SoporteCheckBox.Text = "Soporte";
+            this.SoporteCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // UsuarioCheckBox
+            // 
+            this.UsuarioCheckBox.AutoSize = true;
+            this.UsuarioCheckBox.Location = new System.Drawing.Point(77, 34);
+            this.UsuarioCheckBox.Name = "UsuarioCheckBox";
+            this.UsuarioCheckBox.Size = new System.Drawing.Size(62, 17);
+            this.UsuarioCheckBox.TabIndex = 23;
+            this.UsuarioCheckBox.Text = "Usuario";
+            this.UsuarioCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // NivelDeUsuarioGroupBox
+            // 
+            this.NivelDeUsuarioGroupBox.Controls.Add(this.UsuarioCheckBox);
+            this.NivelDeUsuarioGroupBox.Controls.Add(this.AdministradorCheckBox);
+            this.NivelDeUsuarioGroupBox.Controls.Add(this.SupervisorCheckBox);
+            this.NivelDeUsuarioGroupBox.Controls.Add(this.SoporteCheckBox);
+            this.NivelDeUsuarioGroupBox.Location = new System.Drawing.Point(167, 145);
+            this.NivelDeUsuarioGroupBox.Name = "NivelDeUsuarioGroupBox";
+            this.NivelDeUsuarioGroupBox.Size = new System.Drawing.Size(262, 100);
+            this.NivelDeUsuarioGroupBox.TabIndex = 24;
+            this.NivelDeUsuarioGroupBox.TabStop = false;
+            this.NivelDeUsuarioGroupBox.Text = "groupBox1";
+            // 
+            // RegistroUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.Controls.Add(this.NivelDeUsuarioGroupBox);
+            this.Controls.Add(this.FechaDeIngresoDateTimePicker);
+            this.Controls.Add(this.ClaveTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.UsuarioTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ApellidoTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.NombreTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.IdUsuarioNumericUpDown);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.BuscarButton);
+            this.Name = "RegistroUsuarios";
+            this.Text = "RegistroUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.IdUsuarioNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).EndInit();
+            this.NivelDeUsuarioGroupBox.ResumeLayout(false);
+            this.NivelDeUsuarioGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown IdUsuarioNumericUpDown;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.TextBox ApellidoTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox UsuarioTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox ClaveTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker FechaDeIngresoDateTimePicker;
+        private System.Windows.Forms.ErrorProvider MyErrorProvider;
+        private System.Windows.Forms.CheckBox SoporteCheckBox;
+        private System.Windows.Forms.CheckBox SupervisorCheckBox;
+        private System.Windows.Forms.CheckBox AdministradorCheckBox;
+        private System.Windows.Forms.CheckBox UsuarioCheckBox;
+        private System.Windows.Forms.GroupBox NivelDeUsuarioGroupBox;
+    }
+}
---
+++ ProyectoFinal/UI/Registros/RegistroUsuarios.cs
@@ -0,0 +1,196 @@
+﻿using ProyectoFinal.BLL;
+using ProyectoFinal.Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal.UI.Registros
+{
+    public partial class RegistroUsuarios : Form
+    {
+        public RegistroUsuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void Limpiar()
+        {
+            IdUsuarioNumericUpDown.Value = 0;
+            NombreTextBox.Text = String.Empty;
+            ApellidoTextBox.Text = string.Empty;
+            EmailTextBox.Text = string.Empty;
+            AdministradorCheckBox.Checked = false;
+            SupervisorCheckBox.Checked = false;
+            SoporteCheckBox.Checked = false;
+            UsuarioCheckBox.Checked = false;
+            UsuarioTextBox.Text = string.Empty;
+            ClaveTextBox.Text = string.Empty;
+            FechaDeIngresoDateTimePicker.Value = DateTime.Now;
+        }
+
+        private Usuarios LlenaClase()
+        {
+            Usuarios usuario = new Usuarios();
+            usuario.IdUsuario = Convert.ToInt32(IdUsuarioNumericUpDown.Value);
+            usuario.Nombre = NombreTextBox.Text;
+            usuario.Apellido = ApellidoTextBox.Text;
+            usuario.Email = EmailTextBox.Text;
+            if (usuario.NivelDeUsuario == 1)
+                AdministradorCheckBox.Checked = true;
+            else if (usuario.NivelDeUsuario == 2)
+                SupervisorCheckBox.Checked = true;
+            else if (usuario.NivelDeUsuario == 3)
+                SoporteCheckBox.Checked = true;
+            else
+                UsuarioCheckBox.Checked = true;
+            usuario.Usuario = UsuarioTextBox.Text;
+            usuario.Clave = ClaveTextBox.Text;
+            usuario.FechaDeIngreso = FechaDeIngresoDateTimePicker.Value;
+            return usuario;
+        }
+
+        private void LlenaCampos(Usuarios usuario)
+        {
+            IdUsuarioNumericUpDown.Value = usuario.IdUsuario;
+            NombreTextBox.Text = usuario.Nombre;
+            ApellidoTextBox.Text = usuario.Apellido;
+            EmailTextBox.Text = usuario.Email;
+            if (usuario.NivelDeUsuario == 1)
+                AdministradorCheckBox.Checked = true;
+            else if (usuario.NivelDeUsuario == 2)
+                SupervisorCheckBox.Checked = true;
+            else if (usuario.NivelDeUsuario == 3)
+                SoporteCheckBox.Checked = true;
+            else
+                UsuarioCheckBox.Checked = true;
+            UsuarioTextBox.Text = usuario.Usuario;
+            ClaveTextBox.Text = usuario.Clave;
+            FechaDeIngresoDateTimePicker.Value = usuario.FechaDeIngreso;
+        }
+
+        private bool Validar()
+        {
+            bool paso = true;
+            MyErrorProvider.Clear();
+            if (NombreTextBox.Text == string.Empty)
+            {
+                MyErrorProvider.SetError(NombreTextBox, "El campo \"Nombre\" no puede estar vacio");
+                NombreTextBox.Focus();
+                paso = false;
+            }
+
+            if (ApellidoTextBox.Text == string.Empty)
+            {
+                MyErrorProvider.SetError(ApellidoTextBox, "El campo \"Apellido\" no puede estar vacio");
+                ApellidoTextBox.Focus();
+                paso = false;
+            }
+
+            if (AdministradorCheckBox.Checked == false && SupervisorCheckBox.Checked == false && SoporteCheckBox.Checked == false && UsuarioCheckBox.Checked == false)
+            {
+                MyErrorProvider.SetError(NivelDeUsuarioGroupBox, "Debe elegir un tipo de usuario");
+                NivelDeUsuarioGroupBox.Focus();
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(UsuarioTextBox.Text))
+            {
+                MyErrorProvider.SetError(UsuarioTextBox, "El campo \"Usuario\" no puede estar vacio");
+                UsuarioTextBox.Focus();
+                paso = false;
+            }
+
+            if (string.IsNullOrWhiteSpace(ClaveTextBox.Text))
+            {
+                MyErrorProvider.SetError(ClaveTextBox, "El campo \"Clave\" no puede estar vacio");
+                ClaveTextBox.Focus();
+                paso = false;
+            }
+            return paso;
+        }
+
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            Usuarios usuarios = UsuariosBLL.Buscar((int)IdUsuarioNumericUpDown.Value);
+            return usuarios != null;
+        }
+        private void BuscarButton_Click(object sender, EventArgs e)
+        {
+            int id;
+
+            Usuarios usuario = new Usuarios();
+            int.TryParse(IdUsuarioNumericUpDown.Text, out id);
+            Limpiar();
+            usuario = UsuariosBLL.Buscar(id);
+
+            if (usuario != null)
+            {
+                MessageBox.Show("Usuario encontrad");
+                LlenaCampos(usuario);
+            }
+            else
+                MessageBox.Show("Usuario no encontrado");
+        }
+        private void NuevoButton_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+            NombreTextBox.Focus();
+        }
+
+        private void GuardarButton_Click(object sender, EventArgs e)
+        {
+            Usuarios usuario;
+            bool paso = false;
+
+            if (!Validar())
+                return;
+
+            usuario = LlenaClase();
+            Limpiar();
+
+            if (IdUsuarioNumericUpDown.Value == 0)
+                paso = UsuariosBLL.Guardar(usuario);
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+                    MessageBox.Show("No se puede modificar una persona que no existe");
+                    return;
+                }
+                paso = UsuariosBLL.Modificar(usuario);
+            }
+
+            if (paso)
+                MessageBox.Show("Guardado!", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MessageBox.Show("Error al guardar", "Fallo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void EliminarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdministradorCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            MyErrorProvider.Clear();
+
+            int id;
+            int.TryParse(IdUsuarioNumericUpDown.Text, out id);
+
+            Limpiar();
+
+            if (UsuariosBLL.Eliminar(id))
+                MessageBox.Show("Eliminado", "Exito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                MyErrorProvider.SetError(IdUsuarioNumericUpDown, "No se puede eliminar un usuario que no existe");
+        }
+
+
+    }
+}
