c4832c791cb853be08ea3f2275b1af20bf1758a1
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-21 23:05:13 -0400

    Terminando con las observaciones del registro de usuarios

--- BlacksmithManager/UI/Registros/RUsuarios.Designer.cs
+++ BlacksmithManager/UI/Registros/RUsuarios.Designer.cs
@@ -44,21 +44,19 @@
             this.FechaDeIngresoDateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.MyErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
             this.NivelDeUsuarioGroupBox = new System.Windows.Forms.GroupBox();
-            this.AdministradorRadioButton = new System.Windows.Forms.RadioButton();
-            this.SupervisorRadioButton = new System.Windows.Forms.RadioButton();
-            this.SoporteRadioButton = new System.Windows.Forms.RadioButton();
             this.UsuarioRadioButton = new System.Windows.Forms.RadioButton();
+            this.SoporteRadioButton = new System.Windows.Forms.RadioButton();
+            this.SupervisorRadioButton = new System.Windows.Forms.RadioButton();
+            this.AdministradorRadioButton = new System.Windows.Forms.RadioButton();
             this.Apellido = new System.Windows.Forms.Label();
             this.ID = new System.Windows.Forms.Label();
             this.Email = new System.Windows.Forms.Label();
             this.Usuario = new System.Windows.Forms.Label();
             this.Clave = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.IdUsuarioNumericUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).BeginInit();
             this.NivelDeUsuarioGroupBox.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BuscarButton
@@ -66,9 +64,9 @@
             this.BuscarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BuscarButton.Image = ((System.Drawing.Image)(resources.GetObject("BuscarButton.Image")));
             this.BuscarButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.BuscarButton.Location = new System.Drawing.Point(334, 21);
+            this.BuscarButton.Location = new System.Drawing.Point(305, 31);
             this.BuscarButton.Name = "BuscarButton";
-            this.BuscarButton.Size = new System.Drawing.Size(71, 61);
+            this.BuscarButton.Size = new System.Drawing.Size(68, 53);
             this.BuscarButton.TabIndex = 0;
             this.BuscarButton.Text = "Buscar";
             this.BuscarButton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -80,7 +78,7 @@
             this.NuevoButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NuevoButton.Image = ((System.Drawing.Image)(resources.GetObject("NuevoButton.Image")));
             this.NuevoButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.NuevoButton.Location = new System.Drawing.Point(28, 20);
+            this.NuevoButton.Location = new System.Drawing.Point(15, 284);
             this.NuevoButton.Name = "NuevoButton";
             this.NuevoButton.Size = new System.Drawing.Size(90, 30);
             this.NuevoButton.TabIndex = 1;
@@ -94,7 +92,7 @@
             this.GuardarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.GuardarButton.Image = ((System.Drawing.Image)(resources.GetObject("GuardarButton.Image")));
             this.GuardarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.GuardarButton.Location = new System.Drawing.Point(28, 75);
+            this.GuardarButton.Location = new System.Drawing.Point(151, 284);
             this.GuardarButton.Name = "GuardarButton";
             this.GuardarButton.Size = new System.Drawing.Size(90, 30);
             this.GuardarButton.TabIndex = 2;
@@ -108,7 +106,7 @@
             this.EliminarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.EliminarButton.Image = ((System.Drawing.Image)(resources.GetObject("EliminarButton.Image")));
             this.EliminarButton.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.EliminarButton.Location = new System.Drawing.Point(28, 130);
+            this.EliminarButton.Location = new System.Drawing.Point(283, 284);
             this.EliminarButton.Name = "EliminarButton";
             this.EliminarButton.Size = new System.Drawing.Size(90, 30);
             this.EliminarButton.TabIndex = 3;
@@ -139,7 +137,7 @@
             // 
             this.NombreTextBox.Location = new System.Drawing.Point(109, 54);
             this.NombreTextBox.Name = "NombreTextBox";
-            this.NombreTextBox.Size = new System.Drawing.Size(200, 20);
+            this.NombreTextBox.Size = new System.Drawing.Size(178, 20);
             this.NombreTextBox.TabIndex = 9;
             // 
             // ApellidoTextBox
@@ -147,7 +145,7 @@
             this.ApellidoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ApellidoTextBox.Location = new System.Drawing.Point(109, 87);
             this.ApellidoTextBox.Name = "ApellidoTextBox";
-            this.ApellidoTextBox.Size = new System.Drawing.Size(200, 21);
+            this.ApellidoTextBox.Size = new System.Drawing.Size(178, 21);
             this.ApellidoTextBox.TabIndex = 11;
             // 
             // EmailTextBox
@@ -155,13 +153,13 @@
             this.EmailTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.EmailTextBox.Location = new System.Drawing.Point(109, 122);
             this.EmailTextBox.Name = "EmailTextBox";
-            this.EmailTextBox.Size = new System.Drawing.Size(296, 21);
+            this.EmailTextBox.Size = new System.Drawing.Size(264, 21);
             this.EmailTextBox.TabIndex = 13;
             // 
             // UsuarioTextBox
             // 
             this.UsuarioTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UsuarioTextBox.Location = new System.Drawing.Point(109, 247);
+            this.UsuarioTextBox.Location = new System.Drawing.Point(256, 160);
             this.UsuarioTextBox.Name = "UsuarioTextBox";
             this.UsuarioTextBox.Size = new System.Drawing.Size(117, 21);
             this.UsuarioTextBox.TabIndex = 15;
@@ -169,7 +167,7 @@
             // ClaveTextBox
             // 
             this.ClaveTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ClaveTextBox.Location = new System.Drawing.Point(109, 282);
+            this.ClaveTextBox.Location = new System.Drawing.Point(256, 195);
             this.ClaveTextBox.Name = "ClaveTextBox";
             this.ClaveTextBox.Size = new System.Drawing.Size(117, 21);
             this.ClaveTextBox.TabIndex = 17;
@@ -179,7 +177,7 @@
             this.FechaDeIngresoDateTimePicker.CustomFormat = "dd/MM/yy";
             this.FechaDeIngresoDateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FechaDeIngresoDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.FechaDeIngresoDateTimePicker.Location = new System.Drawing.Point(146, 315);
+            this.FechaDeIngresoDateTimePicker.Location = new System.Drawing.Point(293, 230);
             this.FechaDeIngresoDateTimePicker.Name = "FechaDeIngresoDateTimePicker";
             this.FechaDeIngresoDateTimePicker.Size = new System.Drawing.Size(80, 21);
             this.FechaDeIngresoDateTimePicker.TabIndex = 18;
@@ -197,10 +195,43 @@
             this.NivelDeUsuarioGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NivelDeUsuarioGroupBox.Location = new System.Drawing.Point(15, 160);
             this.NivelDeUsuarioGroupBox.Name = "NivelDeUsuarioGroupBox";
-            this.NivelDeUsuarioGroupBox.Size = new System.Drawing.Size(211, 73);
+            this.NivelDeUsuarioGroupBox.Size = new System.Drawing.Size(131, 108);
             this.NivelDeUsuarioGroupBox.TabIndex = 24;
             this.NivelDeUsuarioGroupBox.TabStop = false;
             this.NivelDeUsuarioGroupBox.Text = "Nivel de usuario";
+            // 
+            // UsuarioRadioButton
+            // 
+            this.UsuarioRadioButton.AutoSize = true;
+            this.UsuarioRadioButton.Location = new System.Drawing.Point(6, 80);
+            this.UsuarioRadioButton.Name = "UsuarioRadioButton";
+            this.UsuarioRadioButton.Size = new System.Drawing.Size(75, 19);
+            this.UsuarioRadioButton.TabIndex = 27;
+            this.UsuarioRadioButton.TabStop = true;
+            this.UsuarioRadioButton.Text = "Usuario";
+            this.UsuarioRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // SoporteRadioButton
+            // 
+            this.SoporteRadioButton.AutoSize = true;
+            this.SoporteRadioButton.Location = new System.Drawing.Point(6, 60);
+            this.SoporteRadioButton.Name = "SoporteRadioButton";
+            this.SoporteRadioButton.Size = new System.Drawing.Size(75, 19);
+            this.SoporteRadioButton.TabIndex = 26;
+            this.SoporteRadioButton.TabStop = true;
+            this.SoporteRadioButton.Text = "Soporte";
+            this.SoporteRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // SupervisorRadioButton
+            // 
+            this.SupervisorRadioButton.AutoSize = true;
+            this.SupervisorRadioButton.Location = new System.Drawing.Point(6, 40);
+            this.SupervisorRadioButton.Name = "SupervisorRadioButton";
+            this.SupervisorRadioButton.Size = new System.Drawing.Size(93, 19);
+            this.SupervisorRadioButton.TabIndex = 25;
+            this.SupervisorRadioButton.TabStop = true;
+            this.SupervisorRadioButton.Text = "Supervisor";
+            this.SupervisorRadioButton.UseVisualStyleBackColor = true;
             // 
             // AdministradorRadioButton
             // 
@@ -212,39 +243,6 @@
             this.AdministradorRadioButton.TabStop = true;
             this.AdministradorRadioButton.Text = "Administrador";
             this.AdministradorRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // SupervisorRadioButton
-            // 
-            this.SupervisorRadioButton.AutoSize = true;
-            this.SupervisorRadioButton.Location = new System.Drawing.Point(6, 45);
-            this.SupervisorRadioButton.Name = "SupervisorRadioButton";
-            this.SupervisorRadioButton.Size = new System.Drawing.Size(93, 19);
-            this.SupervisorRadioButton.TabIndex = 25;
-            this.SupervisorRadioButton.TabStop = true;
-            this.SupervisorRadioButton.Text = "Supervisor";
-            this.SupervisorRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // SoporteRadioButton
-            // 
-            this.SoporteRadioButton.AutoSize = true;
-            this.SoporteRadioButton.Location = new System.Drawing.Point(126, 20);
-            this.SoporteRadioButton.Name = "SoporteRadioButton";
-            this.SoporteRadioButton.Size = new System.Drawing.Size(75, 19);
-            this.SoporteRadioButton.TabIndex = 26;
-            this.SoporteRadioButton.TabStop = true;
-            this.SoporteRadioButton.Text = "Soporte";
-            this.SoporteRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // UsuarioRadioButton
-            // 
-            this.UsuarioRadioButton.AutoSize = true;
-            this.UsuarioRadioButton.Location = new System.Drawing.Point(126, 45);
-            this.UsuarioRadioButton.Name = "UsuarioRadioButton";
-            this.UsuarioRadioButton.Size = new System.Drawing.Size(75, 19);
-            this.UsuarioRadioButton.TabIndex = 27;
-            this.UsuarioRadioButton.TabStop = true;
-            this.UsuarioRadioButton.Text = "Usuario";
-            this.UsuarioRadioButton.UseVisualStyleBackColor = true;
             // 
             // Apellido
             // 
@@ -280,7 +278,7 @@
             // 
             this.Usuario.AutoSize = true;
             this.Usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Usuario.Location = new System.Drawing.Point(18, 250);
+            this.Usuario.Location = new System.Drawing.Point(165, 163);
             this.Usuario.Name = "Usuario";
             this.Usuario.Size = new System.Drawing.Size(61, 15);
             this.Usuario.TabIndex = 28;
@@ -290,7 +288,7 @@
             // 
             this.Clave.AutoSize = true;
             this.Clave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Clave.Location = new System.Drawing.Point(18, 285);
+            this.Clave.Location = new System.Drawing.Point(165, 198);
             this.Clave.Name = "Clave";
             this.Clave.Size = new System.Drawing.Size(46, 15);
             this.Clave.TabIndex = 29;
@@ -300,28 +298,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(18, 320);
+            this.label1.Location = new System.Drawing.Point(165, 233);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(122, 15);
             this.label1.TabIndex = 30;
             this.label1.Text = "Fecha de ingreso:";
             // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.NuevoButton);
-            this.panel1.Controls.Add(this.GuardarButton);
-            this.panel1.Controls.Add(this.EliminarButton);
-            this.panel1.Location = new System.Drawing.Point(260, 160);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(145, 180);
-            this.panel1.TabIndex = 31;
-            // 
-            // RegistroUsuarios
+            // RUsuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 364);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(390, 328);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.NuevoButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Clave);
             this.Controls.Add(this.Usuario);
@@ -339,13 +329,12 @@
             this.Controls.Add(this.Nombre);
             this.Controls.Add(this.BuscarButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "RegistroUsuarios";
+            this.Name = "RUsuarios";
             this.Text = "Registro de usuarios";
             ((System.ComponentModel.ISupportInitialize)(this.IdUsuarioNumericUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).EndInit();
             this.NivelDeUsuarioGroupBox.ResumeLayout(false);
             this.NivelDeUsuarioGroupBox.PerformLayout();
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -376,7 +365,6 @@
         private System.Windows.Forms.Label ID;
         private System.Windows.Forms.Label Clave;
         private System.Windows.Forms.Label Usuario;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
     }
 }
