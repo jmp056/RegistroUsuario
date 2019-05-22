1ef9072cf056c87be9cfbdb5943d4f41515da0c1
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-05-21 23:38:43 -0400

    Agregando la entidad Cargos

---
+++ BlacksmithManager/Entidades/Cargos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Entidades
+{
+    public class Cargos
+    {
+        [Key]
+        public int IdCargo { get; set; }
+        public string DescripcionCargo { get; set; }
+
+        public Cargos()
+        {
+            IdCargo = 0;
+            DescripcionCargo = string.Empty;
+        }
+
+
+    }
+}
